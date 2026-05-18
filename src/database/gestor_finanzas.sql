fc91987c39e9b65b40ad9c9e079c3697e9338ff9
EsmeraldaMTZV <23308060610235@cetis61.edu.mx>
2026-05-18 09:03:49 -0600

    a

--- src/database/gestor_finanzas.sql
+++ src/database/gestor_finanzas.sql
@@ -88,6 +88,7 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
   `fecha_registro` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
